829dac5b31a8dd5b51d2444136beca72b0bdd027
Samuel <samuel.souza@sempreceub.com>
2025-11-12 21:50:45 -0300

    atualizado

--- Função/Arrays/funcaolista.py
+++ Função/Arrays/funcaolista.py
@@ -1,6 +1,6 @@
 # CRUD acrônimo de Create, Read, Update and Delete (inclui, consulta, atualiza e apaga os dados)
 l_nomes = []
-def menu(): 
+def menu():
     print('[c] - Create')
     print('[r] - Read')
     print('[u] - Update')
@@ -9,23 +9,39 @@ def menu():
     opcao = input("Opção: ")
     return opcao
 def create():
-    print('Em desenvolvimento - create')
+    ct=0
+    while True:
+        nome = str(input("Digite o nome: "))
+        if nome != 0:
+            ct+=1
+        if nome == '0':
+            break
+        l_nomes.append(nome)
+    print(f"Quantidades de nomes adicionados:{ct}:")
 def read():
-    print('Em desenvolvimento - read')
+    print(f'O nome na lista é:{l_nomes}')
 def update():
-    print('Em desenvolvimento - update')
+    att = str(input('digite um novo nome:'))
+    nv_nm = int(input('digite a posição do nome'))
+    l_nomes[nv_nm] = att
 def delete():
-    print('Em desenvolvimento - delete')
-if __name__ == 'main':
+    escolha = int(input("ESCOLHA:\n [1] para apagar pelo nome:\n [2] para apagar pelo indíce:\n"))
+    if escolha == 1:
+        dell = str(input('digite o nome que deseja remover:'))
+        l_nomes.remove(dell)
+    else:
+        dell1 = int(input('digite a possição do nome a ser deletado:'))
+        l_nomes.pop(dell1)
+if __name__ == '__main__':
     while True:
         op = menu()
         if op == 'c':
             create()
-        if op == 'r':
+        elif op == 'r':
             read()
-        if op == 'u':
+        elif op == 'u':
             update()
-        if op == 'd':
+        elif op == 'd':
             delete()
         else:
             break
